76425e2764a2019072782123750f254841d8be93
Daniel Ruiz Vargas <danielkenobi3d@gmail.com>
2024-10-02 18:38:17 -0400

    added cubes to the sin function

--- class/Daniel/sinFunction.py
+++ class/Daniel/sinFunction.py
@@ -1,9 +1,9 @@
 from operator import index
-
+import math
 import pymel.core as pm
 import math
 locators = []
-for each in range (20):
+for each in range (60):
     locators.append(pm.spaceLocator())
 amplitude = 5
 phase = 0# math.pi
@@ -14,6 +14,15 @@ resolution_x = 10 / samples_number #this value is in maya units
 math.sin(math.radians(90))
 
 for index, sample_loc in enumerate(locators):
+    transform_cube, cube_creation = pm.polyCube()
+
     sample_loc.translateX.set(resolution_x * index)
+    transform_cube.translateX.set(resolution_x * index)
     y_position = amplitude * math.sin(resolution_a * index + phase) + offset
+    if math.fabs(y_position)<0.1:
+        height=0.1
+    else:
+        height = math.fabs(y_position)
+    cube_creation.height.set(height)
     sample_loc.translateY.set(y_position)
+    transform_cube.translateY.set(y_position/2)
