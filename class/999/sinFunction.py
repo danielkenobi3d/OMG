f7ab08534287cf2b4351ef6f1d591c9a0210e24c
KarlZ999 <9o6o3649o@gmail.com>
2024-10-02 20:53:00 -0400

    Create sinFunction.py

---
+++ class/999/sinFunction.py
@@ -0,0 +1,18 @@
+import pymel.core as pm
+import math
+
+locators = []
+for each in range (20):
+    locators.append(pm.spaceLocator())
+amplitud = 5
+phase = 0 #math.pi
+offset = 0
+samples_number = 20
+resolution_a = math.pi / samples_number #this va;ue is in degrees
+resolution_x = 10 / samples_number # this value is in maya units
+math.sin(math.radians(90))
+
+for index, sample_loc in enumerate(locators):
+    sample_loc.translateX.set(resolution_x * index)
+    y_position = amplitud * math.sin(resolution_a * index + phase) + offset
+    sample_loc.translateY.set(y_position)
