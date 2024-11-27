86e0bc86929bdf134d9b4f191a5b730e5ebb1efb
Daniel Ruiz Vargas <danielkenobi3d@gmail.com>
2024-11-27 18:33:31 -0500

    Update RMPY_basic_rigs.py

--- class/Daniel/RMPY_basic_rigs.py
+++ class/Daniel/RMPY_basic_rigs.py
@@ -1,4 +1,5 @@
 from RMPY.rig import  rigFK
-
-my_rig_fk = rigFK.RigIkFk()
-my_rig_fk.create_point_base()
+import pymel.core as pm
+root_point = pm.ls('C_main00_reference_grp')[0]
+my_rig_fk = rigFK.RigFK()
+my_rig_fk.create_point_base(*root_point.getChildren())
