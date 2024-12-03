63e53a7a13dd1587f625840d300258d052827bc1
Seldon552 <m.bouquot@rubika-edu.ca>
2024-12-02 20:52:41 -0500

    fgd

--- rigBuilds/assets/Intern/rig_build.py
+++ rigBuilds/assets/Intern/rig_build.py
@@ -1,9 +1,12 @@
 from OMG.rigBuilds.assets.Intern.custom_rig import rigBiped
+from OMG.rigBuilds.assets.Intern.custom_rig import rigArm
+import importlib
+importlib.reload(rigArm)
 import pymel.core as pm
 
 
 def build_biped_rig():
-    rig_biped = rigBiped.RigByped()
-    rig_biped.build()
+    rig_arm = rigArm.RigArm()
+    rig_arm.build()
     pm.parent('geo', 'rig')
     pm.parent('environment', 'rig')
