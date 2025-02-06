a1d6c35cd5d1b7fea65e6b990525e4c007749ff5
Daniel Ruiz Vargas <daniel@rendermotion.com>
2025-02-06 11:07:14 -0500

    FIx the script to build the arms

---
+++ rigBuilds/assets/Intern/custom_rig/armSpaceSwitch.py
@@ -0,0 +1,46 @@
+from RMPY.rig import rigBase
+from RMPY.rig.spaceSwitches import rigEnumSpaceSwitch
+
+
+class ArmSpaceSwitch(rigBase.RigBase):
+    def __init__(self, *args, **kwargs):
+        super(ArmSpaceSwitch, self).__init__(*args, **kwargs)
+
+    def build(self, arm_rig, world_rig, cog_rig):
+        # Creates the ik control spaceswitch
+
+        """ replace the world_rig.tip for the object that you want to be the world"""
+        """ Also the cog_rig.tip"""
+
+        enum_space_switch = rigEnumSpaceSwitch.RigEnumSpaceSwitch(world_rig.tip, arm_rig.rig_clavicle.tip,
+                                                                  alias_list=['world', 'shoulder'],
+                                                                  attribute_name='space',
+                                                                  control=arm_rig.rig_arm.ik_rig.controls[0],
+                                                                  parent=True, rig_system=arm_rig.rig_system)
+        enum_space_switch.add_object(arm_rig.rig_arm.ik_rig.controls[0])
+        # Creates the ik pole vector control spaceswitch
+        enum_space_switch = rigEnumSpaceSwitch.RigEnumSpaceSwitch(world_rig.tip, arm_rig.rig_clavicle.tip, arm_rig.rig_arm.ik_rig.controls[0],
+                                                                  alias_list=['world', 'shoulder', 'ikHandle'],
+                                                                  attribute_name='space',
+                                                                  control=arm_rig.rig_arm.ik_rig.controls[1],
+                                                                  parent=True,
+                                                                  rig_system=arm_rig.rig_system)
+        enum_space_switch.add_object(arm_rig.rig_arm.ik_rig.controls[1])
+        # creates the fk control spaceswitch
+        enum_space_switch = rigEnumSpaceSwitch.RigEnumSpaceSwitch(arm_rig.rig_clavicle.tip, world_rig.tip,
+                                                                  cog_rig.tip,
+                                                                  alias_list=['shoulder', 'world', 'hip'],
+                                                                  attribute_name='space',
+                                                                  control=arm_rig.rig_arm.fk_rig.controls[0],
+                                                                  orient=True,
+                                                                  rig_system=arm_rig.rig_system)
+        enum_space_switch.add_object(arm_rig.rig_arm.fk_rig.controls[0])
+
+
+if __name__ == '__main__':
+    enum_space_switch = rigEnumSpaceSwitch.RigEnumSpaceSwitch('C_control02_world_ctr', 'R_clavicle01_clavicle_jnt',
+                                                              alias_list=['world', 'shoulder'],
+                                                              attribute_name='space',
+                                                              control='R_jointIK00_shoulder_ctr',
+                                                              parent=True)
+    enum_space_switch.add_object('R_jointIK00_shoulder_ctr', mo=True)
--- rigBuilds/assets/Intern/custom_rig/rigBiped.py
+++ rigBuilds/assets/Intern/custom_rig/rigBiped.py
@@ -18,7 +18,7 @@ from RMPY.rig.biped.rig import neckHeadSpaceSwitch
 from RMPY.rig.biped.rig import rigEyesSpaceSwitch
 from RMPY.rig import rigSingleJoint
 from RMPY.rig import rigOutput
-
+import pymel.core as pm
 
 class RigBypedModel(rigBase.BaseModel):
     def __init__(self, **kwargs):
@@ -79,23 +79,24 @@ class RigByped(rigBase.RigBase):
 
 
     def build(self):
+
         self.l_arm.create_point_base(*[each.format('L') for each in self.arm_root])
-        self.l_arm.set_parent('shoulder_bind_JNT', create_hierarchy_joints=False)
+        self.l_arm.set_parent(pm.ls('shoulder_bind_JNT')[0], create_hierarchy_joints=True, output_joint_rig = self.l_rig_output)
 
         self.r_arm.create_point_base(*[each.format('R') for each in self.arm_root])
-        self.r_arm.set_parent('shoulder_bind_JNT', create_hierarchy_joints=False)
+        self.r_arm.set_parent(pm.ls('shoulder_bind_JNT')[0], create_hierarchy_joints=True, output_joint_rig = self.r_rig_output)
 
         self.l_hand.create_point_base(*[each.format('L') for each in self.hand_root])
-        self.l_hand.set_parent(self.l_arm, create_hierarchy_joints=False)
+        self.l_hand.set_parent(self.l_arm, create_hierarchy_joints=True, output_joint_rig = self.l_rig_output)
 
-        self.l_arm_space_switch.build(self.l_arm, self.rig_world, self.cog)
-        self.l_hand_space_switch.build(self.l_hand, self.rig_world, self.l_arm)
+        # self.l_arm_space_switch.build(self.l_arm, self.rig_world, self.cog)
+        # self.l_hand_space_switch.build(self.l_hand, self.rig_world, self.l_arm)
 
         self.r_hand.create_point_base(*[each.format('R') for each in self.hand_root])
-        self.r_hand.set_parent(self.r_arm, create_hierarchy_joints=False)
+        self.r_hand.set_parent(self.r_arm, create_hierarchy_joints=True, output_joint_rig = self.r_rig_output)
 
-        self.r_arm_space_switch.build(self.r_arm, self.rig_world, self.cog)
-        self.r_hand_space_switch.build(self.r_hand, self.rig_world, self.r_arm)
+        # self.r_arm_space_switch.build(self.r_arm, self.rig_world, self.cog)
+        # self.r_hand_space_switch.build(self.r_hand, self.rig_world, self.r_arm)
 
 
 
